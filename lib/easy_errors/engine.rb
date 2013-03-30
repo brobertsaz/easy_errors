a20a0fd96694077313f5d88f7ce139817930843b
brobertsaz <broberts_az@yahoo.com>
2013-03-30 11:38:32 -0700

    added rspec and capybara

--- lib/easy_errors/engine.rb
+++ lib/easy_errors/engine.rb
@@ -1,5 +1,7 @@
 module EasyErrors
   class Engine < ::Rails::Engine
     isolate_namespace EasyErrors
+    config.generators.integration_tool :rspec
+    config.generators.test_framework :rspec
   end
 end
